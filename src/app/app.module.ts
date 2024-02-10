d405edc7a4e99204e1e7b8f7dc9409f0fe8acc80
flipdevflowm <flipdevflow@gmail.com>
2024-02-10 12:19:46 -0300

    init aplication

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+@NgModule({
+  imports: [
+    BrowserModule,
+    MatToolbarModule
+    
+  ],
+   
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
